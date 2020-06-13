45716edfdf26c033640289fda2cf1682e5571b9e
aSilas <silastj@hotmail.com>
2020-06-13 12:08:18 -0300

    Verifição de CPF - ok

--- cpf.js
+++ cpf.js
@@ -1,56 +1,56 @@
-const cpfList = document.querySelectorAll('.cpf li');
-console.log(cpfList);
+// const cpfList = document.querySelectorAll('.cpf li');
+// console.log(cpfList);
 
 //TRANSFORMANDO EM ARRAY
-const arrayCpf = [...cpfList];
+// const arrayCpf = [...cpfList];
 
-console.log(arrayCpf);
+// console.log(arrayCpf);
 
 
 //TRANFORMANDO EM ARRAY e TRAZENDO O VALOR
-function ele(elements) {
-    const arrayele = Array.from(elements);
-    return arrayele.map(element => {
-        return element.innerText;
-    })
-}
-console.log(ele(cpfList));
+// function ele(elements) {
+//     const arrayele = Array.from(elements);
+//     return arrayele.map(element => {
+//         return element.innerText;
+//     })
+// }
+// console.log(ele(cpfList));
 
-const limparCPF = (cpf) => {
-    return cpf.replace(/\D/g, '');
-}
-console.log(limparCPF('121.121.222 33'));
+// const limparCPF = (cpf) => {
+//     return cpf.replace(/\D/g, '');
+// }
+// console.log(limparCPF('121.121.222 33'));
 
-const construirCpf = (cpf) => {
-    return cpf.replace(/(\d{3})(\d{3})(\d{4})(\d{2})/g, '$1.$2.$3-$4');
-}
-console.log(construirCpf('111222333344'));
+// const construirCpf = (cpf) => {
+//     return cpf.replace(/(\d{3})(\d{3})(\d{4})(\d{2})/g, '$1.$2.$3-$4');
+// }
+// console.log(construirCpf('111222333344'));
 
-const formatarCpf = (cpfs) => {
-    return cpfs.map(limparCPF).map(construirCpf);
-}
+// const formatarCpf = (cpfs) => {
+//     return cpfs.map(limparCPF).map(construirCpf);
+// }
 
-const rrCPF = ele(cpfList);
-console.log(formatarCpf(rrCPF));
+// const rrCPF = ele(cpfList);
+// console.log(formatarCpf(rrCPF));
 
-const subsCPFS = (cpfsElements) => {
-    const cpfs = ele(cpfsElements);
-    const cpfformatados = formatarCpf(cpfs);
+// const subsCPFS = (cpfsElements) => {
+//     const cpfs = ele(cpfsElements);
+//     const cpfformatados = formatarCpf(cpfs);
 
-    cpfsElements.forEach((element, index) => {
-        element.innerText = cpfformatados[index];
-    })
+//     cpfsElements.forEach((element, index) => {
+//         element.innerText = cpfformatados[index];
+//     })
 
-    console.log(cpfformatados);
+//     console.log(cpfformatados);
 
-}
-subsCPFS(cpfList);
+// }
+// subsCPFS(cpfList);
 
 // VALIDAR CPFS E CNPJ
 
 import ValidarCpf from './validar-cpf.js';
 
 const cpf = document.querySelector('#cpf');
-const validarCpf = new ValidarCpf(cpf);
+const validarCpf = new ValidarCpf(cpf).iniciar();
 
-console.log(validarCpf.construir('123456789000110'));
+// console.log(validarCpf)
--- validar-cpf.js
+++ validar-cpf.js
@@ -8,13 +8,35 @@ export default class ValidarCpf {
         return cpf.replace(/\D/g, '');
     }
     construir(cpf) {
-        return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{4})(\d{2})/g, '$1.$2.$3.$4-$5');
+        return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3.$4');
     }
     formatar(cpf) {
         const cpfLimpo = this.limpar(cpf);
-        return this.construir();
+        return this.construir(cpfLimpo);
     }
-
+    validar(cpf){
+        const matchCpf = cpf.match(/(?:\d{3}[a.\s]?){3}\d{2}/g);
+        return (matchCpf && matchCpf[0] === cpf);
+    }
+    validarNaMudanca(cpfElement){
+        if(this.validar(cpfElement.value)) {
+            cpfElement.value = this.formatar(cpfElement.value);
+            cpfElement.classList.add('valido');
+            cpfElement.classList.remove('erro');
+        } else {
+            cpfElement.classList.add('erro');
+            cpfElement.classList.remove('valido');
+        }
+        // console.log(this.formatar(cpfElement.value))
+    }
+    adicionarEvento() {
+        this.element.addEventListener('change', () => {
+            this.validarNaMudanca(this.element);
+        })
+    }
+    iniciar(){
+        this.adicionarEvento();
+        return this;
+    }
 }
 
-console.log('teste');
